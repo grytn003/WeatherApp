7bb05a66224eb4d1c1b233e4e1e6bd604e9776ac
grytn003 <grytn003@cougars.csusm.edu>
2017-12-05 21:17:12 -0800

    Add project files.

---
+++ Week9PrismExampleApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using WeatherApp.Views;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync($"MainPage");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+            Container.RegisterTypeForNavigation<MoreInfoPage>();
+		}
+    }
+}
+
---
+++ Week9PrismExampleApp/Views/MoreInfoPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace WeatherApp.Views
+{
+    public partial class MoreInfoPage : ContentPage
+    {
+        public MoreInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
